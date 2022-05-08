9bfb16a06b8efded611e48905301b32745c3b112
yserane <55660730+yserane@users.noreply.github.com>
2022-05-08 18:39:11 -0400

    update changes

--- UI/regood/src/app/services/Item/item.service.ts
+++ UI/regood/src/app/services/Item/item.service.ts
@@ -9,30 +9,34 @@ import { Item } from 'src/app/model/item-model';
 })
 export class ItemService {
   endpoint = 'http://vincentprivatenas.mynetgear.com:7070/reGood/rest';
-  headers = {'access_token': localStorage.getItem("userToken")};
 
   constructor(private httpClient: HttpClient) { }
 
   uploadImage(image: FormData, itemId): Observable<any> {
-    return this.httpClient.post(this.endpoint+'/file?item_id='+itemId, image, {headers: this.headers});
+    let headers = {'access_token': localStorage.getItem("userToken")};
+    return this.httpClient.post(this.endpoint+'/file?item_id='+itemId, image, {headers: headers});
   }
   postItem(itemObj: any): Observable<any> {
-    //
+    let headers = {'access_token': localStorage.getItem("userToken")};
    return this.httpClient.post(this.endpoint+'/item', itemObj,
-   {headers: this.headers});
+   {headers: headers});
 
   }
 
   getItemById():Observable<any>{
-    return this.httpClient.get(this.endpoint+'/item/1',  {headers: this.headers});
+    let headers = {'access_token': localStorage.getItem("userToken")};
+    return this.httpClient.get(this.endpoint+'/item/1',  {headers: headers});
   }
 
   getAllItems(index, range):Observable<any>{
-    return this.httpClient.get(this.endpoint+'/item?row_index='+index+'&range='+range,  {headers: this.headers})
+    let headers = {'access_token': localStorage.getItem("userToken")};
+    return this.httpClient.get(this.endpoint+'/item?row_index='+index+'&range='+range,  {headers: headers})
   }
 
   getItemsSize():Observable<any>{
-    return this.httpClient.get(this.endpoint+'/rest/item_size',  {headers: this.headers})
+    let headers = {'access_token': localStorage.getItem("userToken")};
+
+    return this.httpClient.get(this.endpoint+'/rest/item_size',  {headers: headers})
 
   }
   
